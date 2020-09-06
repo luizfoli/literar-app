6d74af9ed8e33b03ac8942296f7f9c5e01e492e2
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-09-06 16:48:37 -0300

    feat: changes

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { Container } from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
+import { Container, LoaderIcon } from './styles';
 import IconPreload from '../../assets/preload-icon.svg';
 
 export default () => {
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        (async() => {
+            const token = await AsyncStorage.getItem('token');
+            if(token) {
+                //TODO
+            } else {
+                navigation.navigate('SignIn');
+            }
+        })()
+    }, [])
+
+
     return(
         <Container>
             <IconPreload width="100%" height="160" fill="#ffffff" />
+            <LoaderIcon size="large" color="#fff" />
         </Container>
     )
 }
--- src/screens/Preload/styles.js
+++ src/screens/Preload/styles.js
@@ -7,3 +7,7 @@ export const Container = styled.SafeAreaView`
     justify-content: center;
     align-items: center;
 `;
+
+export const LoaderIcon = styled.ActivityIndicator`
+    margin-top: 60px;
+`;
