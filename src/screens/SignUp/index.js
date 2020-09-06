a63058147cb2c77a0cf29762311611faf27c3417
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-09-06 20:29:35 -0300

    feat: Finalizando layout da tela de cadastro

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import {Text} from 'react-native';
-import { Container } from './styles';
+
+import { 
+    Container, 
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold
+} from './styles';
+
+import { useNavigation } from '@react-navigation/native';
+
+import SignInput from '../../components/SignInput';
+
+import BookLoverIcon from '../../assets/book-lover.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
+
+    const navigation = useNavigation();
+
+    handleSignClick = () => {
+        navigation.navigate('SignIn');
+    }
+
     return(
         <Container>
-            <Text>SignUp</Text>
+            <BookLoverIcon width="100%" height="160" fill="#ffffff"/>
+            <InputArea>
+                <SignInput IconSvg={EmailIcon} placeholder="Digite seu e-mail" />
+                <SignInput IconSvg={LockIcon} placeholder="Digite sua senha" />
+                <SignInput IconSvg={LockIcon} placeholder="Confirmer sua senha" />
+                <CustomButton>
+                    <CustomButtonText>Cadastrar-se</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+            <SignMessageButton onPress={handleSignClick}>
+                <SignMessageButtonText>Leitor, já possui uma conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>Vamos logar!</SignMessageButtonTextBold>
+            </SignMessageButton>
         </Container>
     )
 }
