47e7a1134b6b007e73917db05134cf5eebb134b1
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-09-06 16:19:39 -0300

    feat: Finalizando ajustes de confiuracoes

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Text} from 'react-native';
+
 import { Container } from './styles';
+
+import IconPreload from '../../assets/preload-icon.svg';
 
 export default () => {
     return(
         <Container>
-            <Text>Preload</Text>
+            <IconPreload width="100%" height="160" fill="#ffffff" />
         </Container>
     )
 }
