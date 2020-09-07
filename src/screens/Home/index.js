420d4e760209b42e9216b52fa3f86a30e0636d51
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-09-06 21:52:17 -0300

    feat: changes

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import Text from 'react-native';
 
-import {Container} from './styles';
+import {
+    Container,
+    BookStatusContainer,
+    BooksHeaderContainer,
+    BookStatusText,
+    BooksStatusSubText,
+    BooksViewerContainer,
+    Book,
+} from './styles';
 
 export default () => {
     return (
         <Container>
+            <BookStatusContainer>
+                <BooksHeaderContainer>
+                    <BookStatusText>Lendo</BookStatusText>
+                    <BooksStatusSubText>(1)</BooksStatusSubText>
+                </BooksHeaderContainer>
+                <BooksViewerContainer 
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}>
+                    <Book></Book>
+                </BooksViewerContainer>
+            </BookStatusContainer>
+            <BookStatusContainer>
+                <BooksHeaderContainer>
+                    <BookStatusText>Quero Ler</BookStatusText>
+                    <BooksStatusSubText>(4)</BooksStatusSubText>
+                </BooksHeaderContainer>
+                <BooksViewerContainer 
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}>
+                    <Book></Book>
+                    <Book></Book>
+                    <Book></Book>
+                    <Book></Book>
+                </BooksViewerContainer>
+            </BookStatusContainer>
+            <BookStatusContainer>
+                <BooksHeaderContainer>
+                    <BookStatusText>Lido</BookStatusText>
+                    <BooksStatusSubText>(1)</BooksStatusSubText>
+                </BooksHeaderContainer>
+                <BooksViewerContainer 
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                >
+                    <Book></Book>
+                    <Book></Book>
+                    <Book></Book>
+                    <Book></Book>
+                </BooksViewerContainer>
+            </BookStatusContainer>
         </Container>
     );
 };
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -4,6 +4,57 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
     background-color: #3B3B98;
     flex: 1;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
+`;
+
+export const BookStatusContainer = styled.View`
+    width: 90%;
+    height: 25%;
+    margin: 20px;
+`;
+
+export const BooksHeaderContainer = styled.View`
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const BookStatusText = styled.Text`
+    font-size: 26px;
+    color: #fff;
+    font-weight: bold;
+`;
+
+export const BooksStatusSubText  = styled.Text`
+    font-size: 16px;
+    padding: 5px;
+    color: #182C61;
+    font-weight: bold;
+`;
+    
+export const BooksViewerContainer = styled.ScrollView`
+    width: 100%;
+    flex: 1;
+    flex-direction: row;
+    overflow: scroll;
+`;
+    
+export const Book = styled.View`
+    width: 140px;
+    height: 160px;
+    background-color: #fff;
+    margin: 20px 5px;
+`
+
+export const ButtonAddBook = styled.TouchableOpacity`
+    padding: 10px;
+    background-color: #182C61;
+    border-radius: 2px;
+`;
+
+export const ButtonAddBookText = styled.Text`
+    font-size: 18px;
+    color: #fff;
+    text-align: center;
 `;
